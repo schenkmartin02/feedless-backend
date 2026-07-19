604f6d8e1da63193290d61bc4bfca678e306b1de
Schenk Martin <schenkmartin2002@gmail.com>
2026-07-19 11:39:36 +0200

    Add players table migration

---
+++ src/main/resources/db/migration/V1__create_players.sql
@@ -0,0 +1,7 @@
+CREATE TABLE players (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    puuid VARCHAR(78) NOT NULL UNIQUE,
+    game_name VARCHAR(16),
+    tag_line VARCHAR(5),
+    profile_updated_at TIMESTAMPTZ
+);
